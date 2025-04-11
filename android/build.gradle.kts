db30067327444c2422b736694b43fd3bc472807b
urisool <164739999+urisool@users.noreply.github.com>
2025-04-11 15:34:53 +0200

    gradle fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         // تأكد من استخدام النسخة الصحيحة من Gradle و Kotlin
-        classpath("com.android.tools.build:gradle:8.10.2")  // النسخة المتوافقة مع SDK 33
+        classpath ("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")  // النسخة المتوافقة مع Kotlin
         classpath("com.google.gms:google-services:4.3.15")  // إذا كنت تستخدم Firebase
     }
