92a197f2fb51d758a80928f06fbc458b59cf0405
aicha05berraf <berrafaicha5@gmail.com>
2025-04-12 01:11:39 +0200

    graaaaaaaapleeeeeeeeeeee

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,24 +17,8 @@ allprojects {
     }
 }
 
-<<<<<<< HEAD
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath("com.google.gms:google-services:4.3.15")  
-    }
-}
-
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
-=======
-val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.set(newBuildDir)
->>>>>>> b36dbfad87fd2c8682afc5a05940840555293b74
 
 subprojects {
     val newSubprojectBuildDir = newBuildDir.dir(project.name)
